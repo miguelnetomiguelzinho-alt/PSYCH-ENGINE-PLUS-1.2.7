58e17d024083c7e3e9a47fbf32791c866384b941
miguelnetomiguelzinho-alt <miguelnetomiguelzinho@gmail.com>
2026-08-29 10:24:59 -0300

    Atualizar o unish-lib.sh

--- setup/unish-lib.sh
+++ setup/unish-lib.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-# SETUP FOR MAC AND LINUX SYSTEMS!!!
-# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
-# https://haxe.org/download
-cd ..
-echo Makking the main haxelib and setuping folder in same time..
-mkdir ~/haxelib && haxelib setup ~/haxelib
-echo Installing libraries...
-echo This might take a few moments depending on your internet speed.
-haxelib install hxcpp
-haxelib install lime
-haxelib install openfl
-haxelib install openfl 9.5.0 --quiet
-haxelib git flixel https://github.com/kittycathy233/flixel --quiet
+set -eu
+
+echo Installing Haxe libraries...
+mkdir -p \~/haxelib
+haxelib setup \~/haxelib
+
+haxelib git hxcpp https://github.com/Psych-Plus-Team/hxcpp --quiet
+haxelib git lime https://github.com/Psych-Plus-Team/lime --quiet
+haxelib install openfl 9.5.2 --quiet
+haxelib git flixel https://github.com/Psych-Plus-Team/flixel --quiet
 haxelib install flixel-addons 3.3.2 --quiet
+haxelib install flixel-ui 2.6.2 --quiet
 haxelib install moonchart 0.5.1 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
-haxelib install hscript-iris 1.1.3 --quiet
+haxelib git hscript-iris https://github.com/Psych-Plus-Team/hscript-iris --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
-haxelib git linc_luajit https://github.com/Psych-Plus-Team/linc_luajit.git --quiet
+haxelib git linc_luajit https://github.com/Psych-Plus-Team/linc_luajit --quiet
 haxelib install hxdiscord_rpc --quiet --skip-dependencies
-haxelib install hxvlc 2.2.5 --quiet --skip-dependencies
+haxelib install hxvlc 2.2.6 --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 haxelib install extension-androidtools --quiet
-echo Finished!
 
+echo Finished!
